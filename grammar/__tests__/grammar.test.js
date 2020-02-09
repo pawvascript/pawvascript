75a7b12d168f3f33ce7694424a7230797d302916
Moriah Tolliver <mtollive@lion.lmu.edu>
2020-02-09 13:05:28 -0800

    added tests for comparator examples

--- grammar/__tests__/grammar.test.js
+++ grammar/__tests__/grammar.test.js
@@ -11,7 +11,17 @@ const examplePrograms = [
   ['Statement', 'hello world', 'say "Hello, World!";'],
   ['Declaration', 'declare strings', 'bark string dogName is "Cece";'],
   ['Declaration', 'declare numbers', 'bark number dogAge is 12;'],
-  ['Declaration', 'declare booleans', 'bark boolean isCute is true;']
+  ['Declaration', 'declare booleans', 'bark boolean isCute is true;'],
+  ['Declaration', 'declare lists', 'bark list dogNames[string] is ["Cece", "Fluffy"];'],
+  ['Declaration', 'declare maps', 'bark map dogAges[string:number] is ["Cece": 1, "Fluffy": 2];'],
+  ['Declaration', 'declare uninitialized variables', 'bark number cuteness;'],
+  ['Assignment', 'assign uninitialized variables', 'cuteness is 100'],
+  ['EqualityExp', 'check equality', 'x equals y'],
+  ['EqualityExp', 'check inequaltiy', 'x not equals y'],
+  ['EqualityExp', 'check greater than', 'x is greater than y'],
+  ['EqualityExp', 'check less than', 'x is less than y'],
+  ['EqualityExp', 'check greater or equals', 'x is at least y'],
+  ['EqualityExp', 'check less or equals', 'x is at most y'],
 ];
 
 describe('Programs can', () => {
