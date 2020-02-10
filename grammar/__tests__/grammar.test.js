a8298c1336cab95574be63bcd34e25072049d3b8
Lucille Njoo <lucillenjoo@gmail.com>
2020-02-09 22:49:45 -0800

    Add tests for arithmetic and other keywords

--- grammar/__tests__/grammar.test.js
+++ grammar/__tests__/grammar.test.js
@@ -10,9 +10,13 @@ const syntaxCheck = require('../syntax-checker');
 const readmeExamples = [
   ['hello world', 'say "Hello, World!";', 'Statement'],
   ['string declarations', 'bark string dogName is "CeCe";', 'Declaration'],
+  ['string declarations with exclamation points', 'bark string dogName is "CeCe!!!!!!!!!!!";', 'Declaration'],
+  ['string concatenation', 'bark string dogName is "Ce" with "Ce";', 'Statement'],
+  ['string interpolation', 'bark string sentence is "![x] is a good girl";', 'Statement'],
   ['number declarations', 'bark number dogAge is 12;', 'Declaration'],
   ['boolean declarations', 'bark boolean isCute is true;', 'Declaration'],
   ['list declarations', 'bark list dogNames[string] is ["CeCe", "Fluffy"];', 'Declaration'],
+  ['list using without keyword', 'bark list dogNames[string] is ["CeCe", "Fluffy"] without "CeCe";', 'Declaration'],
   ['map declarations', 'bark map dogAges[string:number] is ["CeCe": 1, "Fluffy": 2];', 'Declaration'],
   ['uninitialized variable declarations', 'bark number cuteness;', 'Declaration'],
   ['assigning uninitialized variables', 'cuteness is 100', 'Assignment'],
@@ -20,91 +24,111 @@ const readmeExamples = [
   ['inequaltiy comparators', 'x not equals y', 'EqualityExp'],
   ['greater than comparators', 'x is greater than y', 'EqualityExp'],
   ['less than comparators', 'x is less than y', 'EqualityExp'],
-  ['greater or equals comparators', 'x is at least y', 'EqualityExp'],
-  ['less or equals comparators', 'x is at most y', 'EqualityExp'],
-  ['if, else statements',    `if x is at least y then:
-                                               bark string dogName is "CeCe";
-                                             else:
-                                               bark string dogName is "Fluffy";
-                                                 bark string dogAge is 12;
-                                             end`, 'Statement'],
-  ['if, else if statements', `if x not equals y then:
-                                            say "CeCe is kinda cute";
-                                          else if x is greater than y then:
-                                            say "CeCe is pretty cute";
-                                          else if x is less than y then:
-                                            say "Okay, CeCe is really cute";
-                                          else:
-                                            say "CeCe is the cutest of the cutest";
-                                          end`, 'Statement'],
+  ['greater than or equal to comparators', 'x is at least y', 'EqualityExp'],
+  ['less than or equal to comparators', 'x is at most y', 'EqualityExp'],
+  ['addition', 'x + y', 'Term'],
+  ['subtraction', 'x - y', 'Term'],
+  ['negation', '-x', 'Term'],
+  ['multiplication', 'x * y', 'Term'],
+  ['division', 'x / y', 'Term'],
+  ['modulo operator', 'x mod y', 'Term'],
+  ['factorial', 'x!', 'Term'],  
+  ['if-else statements', 
+   `if x is at least y then:
+        bark string dogName is "CeCe";
+    else:
+        bark string dogName is "Fluffy";
+        bark string dogAge is 12;
+    end`, 
+   'Statement'],
+  ['if-else if statements', 
+   `if x not equals y then:
+        say "CeCe is kinda cute";
+    else if x is greater than y then:
+        say "CeCe is pretty cute";
+    else if x is less than y then:
+        say "Okay, CeCe is really cute";
+    else:
+        say "CeCe is the cutest of the cutest";
+    end`, 
+   'Statement'],
   ['one line comments', `!!! I'm a one line comment !!!`, 'comment'],
   ['multiline comments', `!!! I'm a \n multiline \n comment !!!`, 'comment'],
-  ['forever loops', `loop:
-                                    say "I run forever!";
-                                  end`],
-  ['fixed loops',   `loop 5 times:
-                                      say "Stay.";
-                                    end`],
-  ['while loops',   `loop while x is at most 5:
-                                      say x;
-                                  end`, 'Statement'],
-  ['for loops',     `loop number i is 0 by i*2 while i is less than 10:
-                                    say i;
-                                  end`, 'Statement'],
-  ['for each loops', `loop element through myList:
-                                    say element;
-                                  end`, 'Statement'],
-  ['function declaration', `bark func gcd chases[number:num1, number:num2] fetches number:
-                                            bark number remainder;
-
-                                            loop while (a mod b) is greater than 0:
-                                              remainder is (a mod b);
-                                              a is b;
-                                              b is remainder;
-                                            end
-                                          end`, 'Declaration'],
-  ['object declaration and creation', `bark breed Owner is:
-                                                        bark string dogName;
-                                                          
-                                                          bark func Owner chases[string:dogName] fetches Owner;
-                                                          
-                                                          bark func introduceDog:
-                                                            say "My dog's name is " with Owner's dogName;
-                                                          end
-
-                                                          bark func command fetches string:
-                                                            give Owner's dogName with ", stay.";
-                                                          end
-                                                      end
-                                                      bark Owner lucille is Owner("Cece");
-                                                      lucille's introduceDog();  !!! output: "My dog's name is Cece" !!!
-                                                      say lucille's command(); !!! output: "Cece, stay." !!!
-                                                      `]
+  ['forever loops', 
+   `loop:
+        say "I run forever!";
+    end`],
+  ['fixed loops',
+   `loop 5 times:
+        say "Stay.";
+    end`],
+  ['while loops',
+   `loop while x is at most 5:
+        say x;
+    end`, 
+   'Statement'],
+  ['for loops',
+   `loop number i is 0 by i*2 while i is less than 10:
+        say i;
+    end`, 
+   'Statement'],
+  ['for each loops', 
+   `loop element through myList:
+        say element;
+    end`, 
+   'Statement'],
+  ['function declaration', 
+   `bark func gcd chases[number:num1, number:num2] fetches number:
+        bark number remainder;
+        loop while (a mod b) is greater than 0:
+            remainder is (a mod b);
+            a is b;
+            b is remainder;
+        end
+        give a;
+    end`, 
+   'Declaration'],
+  ['breed declaration and instantiation', 
+   `bark breed Owner is:
+        bark string dogName;
+        bark func Owner chases[string:dogName] fetches Owner;
+        bark func introduceDog:
+            say "My dog's name is " with Owner's dogName;
+        end
+        bark func command fetches string:
+            give Owner's dogName with ", stay.";
+        end
+    end
+    bark Owner lucille is Owner("Cece");
+    lucille's introduceDog();  !!! output: "My dog's name is Cece" !!!
+    say lucille's command(); !!! output: "Cece, stay." !!!
+   `]
                                                                                                                           
 ];
 
 const objects = [
-  ['object declaration', `bark breed Owner is:
-                                          bark string dogName;
-                                        end`, 'Declaration'],
-  ['object constructor declaration', `bark breed Owner is:
-                                                      bark string dogName;
-                                                        
-                                                        bark func Owner chases[string:dogName] fetches Owner;
-                                                    end`, 'Declaration'],
-  ['object with function', `bark breed Owner is:
-                                            bark string dogName;
-                                              
-                                              bark func Owner chases[string:dogName] fetches Owner;
-                                              
-                                              bark func introduceDog:
-                                                say "My dog's name is " with Owner's dogName;
-                                              end
-
-                                              bark func command fetches string:
-                                                give Owner's dogName with ", stay.";
-                                              end
-                                          end`]                                                  
+  ['object declaration', 
+   `bark breed Owner is:
+        bark string dogName;
+    end`, 
+   'Declaration'],
+  ['object constructor declaration', 
+   `bark breed Owner is:
+        bark string dogName;
+        bark func Owner chases[string:dogName] fetches Owner;
+    end`, 
+   'Declaration'],
+  ['object with function', 
+   `bark breed Owner is:
+        bark string dogName;
+        bark func Owner chases[string:dogName] fetches Owner;
+        bark func introduceDog:
+            say "My dog's name is " with Owner's dogName;
+        end
+        bark func command fetches string:
+            give Owner's dogName with ", stay.";
+        end
+    end`]                                                  
 ];
 
 const comments = [
