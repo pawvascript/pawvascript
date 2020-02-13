7a17b2fdf01971ea79776be5f31f92a2490082fb
Lucille Njoo <lucillenjoo@gmail.com>
2020-02-13 13:40:46 -0800

    Update (almost) all tests with new grammar rules; one test still failing

--- grammar/__tests__/grammar.test.js
+++ grammar/__tests__/grammar.test.js
@@ -1,236 +1,282 @@
 /*
  * Grammar Success Test
  *
- * These tests that the PawvaScript grammar accepts a program that features all of
+ * These tests that the PawvaScript grammar accepts a program that features all
  * syntactic forms of the language.
  */
 
-const syntaxCheck = require('../syntax-checker');
+const syntaxCheck = require("../syntax-checker");
 
 const readmeExamples = [
-  ['hello world', 'say "Hello, World!";', 'Statement'],
-  ['string declarations', 'bark string dogName is "CeCe";', 'Declaration'],
-  ['string declarations with exclamation points', 'bark string dogName is "CeCe!!!!!!!!!!!";', 'Declaration'],
-  ['string concatenation', 'bark string dogName is "Ce" with "Ce";', 'Statement'],
-  ['string interpolation', 'bark string sentence is "![x] is a good girl";', 'Statement'],
-  ['number declarations', 'bark number dogAge is 12;', 'Declaration'],
-  ['boolean declarations', 'bark boolean isCute is true;', 'Declaration'],
-  ['list declarations', 'bark list dogNames[string] is ["CeCe", "Fluffy"];', 'Declaration'],
-  ['list using without keyword', 'bark list dogNames[string] is ["CeCe", "Fluffy"] without "CeCe";', 'Declaration'],
-  ['map declarations', 'bark map dogAges[string:number] is ["CeCe": 1, "Fluffy": 2];', 'Declaration'],
-  ['uninitialized variable declarations', 'bark number cuteness;', 'Declaration'],
-  ['assigning uninitialized variables', 'cuteness is 100', 'Assignment'],
-  ['equality comparators', 'x equals y', 'EqualityExp'],
-  ['inequaltiy comparators', 'x not equals y', 'EqualityExp'],
-  ['greater than comparators', 'x is greater than y', 'EqualityExp'],
-  ['less than comparators', 'x is less than y', 'EqualityExp'],
-  ['greater than or equal to comparators', 'x is at least y', 'EqualityExp'],
-  ['less than or equal to comparators', 'x is at most y', 'EqualityExp'],
-  ['addition', 'x + y', 'Term'],
-  ['subtraction', 'x - y', 'Term'],
-  ['negation', '-x', 'Term'],
-  ['multiplication', 'x * y', 'Term'],
-  ['division', 'x / y', 'Term'],
-  ['modulo operator', 'x mod y', 'Term'],
-  ['factorial', 'x!', 'Term'],  
-  ['if-else statements', 
-   `if x is at least y then:
-        bark string dogName is "CeCe";
+  ["hello world", 'woof "Hello, World!";', "Statement"],
+  ["HELLO WORLD", 'bark "Hello, World!";', "Statement"],
+  ["howl errors", 'howl "Error Message";', "Statement"],
+  ["leash declarations", 'leash dogName is "CeCe";', "Declaration"],
+  [
+    "leash declarations with exclamation points",
+    'leash dogName is "CeCe!!!!!!!!!!!";',
+    "Declaration"
+  ],
+  ["leash concatenation", 'leash dogName is "Ce" with "Ce";', "Statement"],
+  [
+    "leash interpolation",
+    'leash sentence is "![x] is a good girl";',
+    "Statement"
+  ],
+  ["toebeans declarations", "toebeans dogAge is 12;", "Declaration"],
+  ["goodBoy declarations", "goodBoy isCute is good;", "Declaration"],
+  [
+    "list declarations",
+    'list dogNames[leash] is ["CeCe", "Fluffy"];',
+    "Declaration"
+  ],
+  [
+    "list using without keyword",
+    'list dogNames[leash] is ["CeCe", "Fluffy"] without "CeCe";',
+    "Declaration"
+  ],
+  [
+    "map declarations",
+    'map dogAges[leash:toebeans] is ["CeCe": 1, "Fluffy": 2];',
+    "Declaration"
+  ],
+  ["uninitialized variable declarations", "toebeans cuteness;", "Declaration"],
+  ["assigning uninitialized variables", "cuteness is 100", "Assignment"],
+  ["equality comparators", "x equals y", "RelopExp"],
+  ["inequaltiy comparators", "x notEquals y", "RelopExp"],
+  ["greater than comparators", "x isGreaterThan y", "RelopExp"],
+  ["less than comparators", "x isLessThan y", "RelopExp"],
+  ["greater than or equal to comparators", "x isAtLeast y", "RelopExp"],
+  ["less than or equal to comparators", "x isAtMost y", "RelopExp"],
+  ["logical negation", "x equals not y", "RelopExp"],
+  ["addition", "x + y", "Term"],
+  ["subtraction", "x - y", "Term"],
+  ["negation", "-x", "Term"],
+  ["multiplication", "x * y", "Term"],
+  ["division", "x / y", "Term"],
+  ["modulo operator", "x mod y", "Term"],
+  ["factorial", "x!", "Term"],
+  [
+    "if-else statements",
+    `if x isAtLeast y then:
+        leash dogName is "CeCe";
     else:
-        bark string dogName is "Fluffy";
-        bark string dogAge is 12;
-    end`, 
-   'Statement'],
-  ['if-else if statements', 
-   `if x not equals y then:
-        say "CeCe is kinda cute";
-    else if x is greater than y then:
-        say "CeCe is pretty cute";
-    else if x is less than y then:
-        say "Okay, CeCe is really cute";
+        leash dogName is "Fluffy";
+        leash dogAge is 12;
+    tail`,
+    "Statement"
+  ],
+  [
+    "if-else if statements",
+    `if x notEquals y then:
+        woof "CeCe is kinda cute";
+    else if x isGreaterThan y then:
+        woof "CeCe is pretty cute";
+    else if x isLessThan y then:
+        woof "Okay, CeCe is really cute";
     else:
-        say "CeCe is the cutest of the cutest";
-    end`, 
-   'Statement'],
-  ['one line comments', `!!! I'm a one line comment !!!`, 'comment'],
-  ['multiline comments', `!!! I'm a \n multiline \n comment !!!`, 'comment'],
-  ['forever loops', 
-   `loop:
-        say "I run forever!";
-    end`],
-  ['fixed loops',
-   `loop 5 times:
-        say "Stay.";
-    end`],
-  ['while loops',
-   `loop while x is at most 5:
-        say x;
-    end`, 
-   'Statement'],
-  ['for loops',
-   `loop number i is 0 by i*2 while i is less than 10:
-        say i;
-    end`, 
-   'Statement'],
-  ['for each loops', 
-   `loop element through myList:
-        say element;
-    end`, 
-   'Statement'],
-  ['function declaration', 
-   `bark func gcd chases[number:num1, number:num2] fetches number:
-        bark number remainder;
-        loop while (a mod b) is greater than 0:
+        woof "CeCe is the cutest of the cutest";
+    tail`,
+    "Statement"
+  ],
+  ["one line comments", `!!! I'm a one line comment !!!`, "comment"],
+  ["multiline comments", `!!! I'm a \n multiline \n comment !!!`, "comment"],
+  [
+    "forever loops",
+    `chase:
+        woof "I run forever!";
+    tail`
+  ],
+  [
+    "fixed loops",
+    `chase 5 times:
+        woof "Stay.";
+    tail`
+  ],
+  [
+    "while loops",
+    `chase while x isAtMost 5:
+        woof x;
+    tail`,
+    "Statement"
+  ],
+  [
+    "for loops",
+    `chase toebeans i is 0 by i*2 while i isLessThan 10:
+        woof i;
+    tail`,
+    "Statement"
+  ],
+  [
+    "for each loops",
+    `chase element through myList:
+        woof element;
+    tail`,
+    "Statement"
+  ],
+  [
+    "function declaration",
+    `trick gcd chews[toebeans:num1, toebeans:num2] fetches toebeans:
+        toebeans remainder;
+        chase while (a mod b) isGreaterThan 0:
             remainder is (a mod b);
             a is b;
             b is remainder;
-        end
+        tail
         give a;
-    end`, 
-   'Declaration'],
-  ['breed declaration and instantiation', 
-   `bark breed Owner is:
-        bark string dogName;
-        bark func Owner chases[string:dogName] fetches Owner;
-        bark func introduceDog:
-            say "My dog's name is " with Owner's dogName;
-        end
-        bark func command fetches string:
+    tail`,
+    "Declaration"
+  ],
+  [
+    "breed declaration and instantiation",
+    `breed Owner is:
+        leash dogName;
+        trick Owner chews[leash:dogName] fetches Owner;
+        trick introduceDog:
+            woof "My dog's name is " with Owner's dogName;
+        tail
+        trick command fetches leash:
             give Owner's dogName with ", stay.";
-        end
-    end
-    bark Owner lucille is Owner("Cece");
+        tail
+    tail
+    Owner lucille is Owner("Cece");
     lucille's introduceDog();  !!! output: "My dog's name is Cece" !!!
-    say lucille's command(); !!! output: "Cece, stay." !!!
-   `]
-                                                                                                                          
+    woof lucille's command(); !!! output: "Cece, stay." !!!
+   `
+  ]
 ];
 
 const objects = [
-  ['object declaration', 
-   `bark breed Owner is:
-        bark string dogName;
-    end`, 
-   'Declaration'],
-  ['object constructor declaration', 
-   `bark breed Owner is:
-        bark string dogName;
-        bark func Owner chases[string:dogName] fetches Owner;
-    end`, 
-   'Declaration'],
-  ['object with function', 
-   `bark breed Owner is:
-        bark string dogName;
-        bark func Owner chases[string:dogName] fetches Owner;
-        bark func introduceDog:
-            say "My dog's name is " with Owner's dogName;
-        end
-        bark func command fetches string:
+  [
+    "object declaration",
+    `breed Owner is:
+        leash dogName;
+    tail`,
+    "Declaration"
+  ],
+  [
+    "object constructor declaration",
+    `breed Owner is:
+        leash dogName;
+        trick Owner chews[leash:dogName] fetches Owner;
+    tail`,
+    "Declaration"
+  ],
+  [
+    "object with methods",
+    `breed Owner is:
+        leash dogName;
+        trick Owner chews[leash:dogName] fetches Owner;
+        trick introduceDog:
+            woof "My dog's name is " with Owner's dogName;
+        tail
+        trick command fetches leash:
             give Owner's dogName with ", stay.";
-        end
-    end`]                                                  
+        tail
+    tail`
+  ]
 ];
 
 const comments = [
-  ['exclamation mark in comment', '!!! I should be a comment! !!!'],
-  ['tags on different lines', `!!!\nComment under tags\nlike so\n!!!`],
-  ['all kinds of characters', '!!! @#$@#%^&{123456;}!!!']
+  ["exclamation mark in comment", "!!! I should be a comment! !!!"],
+  ["tags on different lines", `!!!\nComment under tags\nlike so\n!!!`],
+  ["all kinds of characters", "!!! @#$@#%^&{123456;}!!!"]
 ];
 
 const ids = [
-  ['start with _', '_hello'],
-  ['end with _', 'hello_'],
-  ['_s all over the place', '_oh_hey_there_'],
-  ['has numbers', 'not100'],
-  ['start with keyword', 'withoutDogs'],
-  ['contains keyword', 'living_with_dogs'],
-  ['end with keyword', 'never_barks'],
-  ['starts with type', 'numberOne'],
-  ['one letter', 'm']
-]
+  ["start with an underscore", "_hello"],
+  ["end with an underscore", "hello_"],
+  ["underscores all over the place", "_oh_hey_there_"],
+  ["has numbers", "not100"],
+  ["start with keyword", "withoutDogs"],
+  ["contains keyword", "living_with_dogs"],
+  ["end with keyword", "never_barks"],
+  ["starts with type", "numberOne"],
+  ["one letter", "m"]
+];
 
 const Primaries = [
-  ['parenthesized expression', '(2 * id)'],
-  ['apostrophe operator', "toal's hat"],
-  ['list', `["CeCe", "Fluffy", "Mr. Dog"]`],
-  ['one element list', `["CeCe"]`],
-  ['list within a list', `[["CeCe", "Fluffy"], ["Marvin"]]`],
-  ['empty list', `[]`],
-  ['map', `["CeCe": "cutest", "Marcy": "cute", "Marvin": "barely passing"]`],
-  ['one element map', `["CeCe": "too cute"]`],
-  ['map within a map', `["Cece": ["cuteness": 100]]`],
-  ['list within a map', `["Marvin": ["bleh": "meh"]]`]
+  ["parenthesized expression", "(2 * id)"],
+  ["apostrophe operator", "toal's hat"],
+  ["list", `["CeCe", "Fluffy", "Mr. Dog"]`],
+  ["one element list", `["CeCe"]`],
+  ["list within a list", `[["CeCe", "Fluffy"], ["Marvin"]]`],
+  ["empty list", `[]`],
+  ["map", `["CeCe": "cutest", "Marcy": "cute", "Marvin": "barely passing"]`],
+  ["one element map", `["CeCe": "too cute"]`],
+  ["map within a map", `["Cece": ["cuteness": 100]]`],
+  ["list within a map", `["Marvin": ["bleh": "meh"]]`]
 ];
 
 const Terms = [
-  ['negated', '-9'],
-  ['factorial', '10!'],
-  ['operation before negation', '10 mod -88']
+  ["negated", "-9"],
+  ["factorial", "10!"],
+  ["operation before negation", "10 mod -88"]
 ];
 
 const interpolations = [
-  ['single', `"Hello, my name is ![name]"`],
-  ['mulitple', `"The time is ![hours] : ![minutes] : ![seconds]"`]
+  ["single", `"Hello, my name is ![name]"`],
+  ["mulitple", `"The time is ![hours] : ![minutes] : ![seconds]"`]
 ];
 
-describe('The syntax can match', () => {
-  readmeExamples.forEach( ([scenario, program, startPoint]) => {
-    test(scenario, (done) => {
+describe("The syntax can match", () => {
+  readmeExamples.forEach(([scenario, program, startPoint]) => {
+    test(scenario, done => {
       expect(syntaxCheck(program, startPoint)).toBe(true);
       done();
     });
   });
 });
 
-describe('Declarations can be', () => {
-  objects.forEach( ([scenario, snippet, startPoint]) => {
-    test(scenario, (done) => {
+describe("Declarations can be", () => {
+  objects.forEach(([scenario, snippet, startPoint]) => {
+    test(scenario, done => {
       expect(syntaxCheck(snippet, startPoint)).toBe(true);
       done();
     });
   });
 });
 
-describe('comments match', () => {
-  comments.forEach( ([scenario, snippet]) => {
-    test(scenario, (done) => {
-      expect(syntaxCheck(snippet, 'comment')).toBe(true);
+describe("comments match", () => {
+  comments.forEach(([scenario, snippet]) => {
+    test(scenario, done => {
+      expect(syntaxCheck(snippet, "comment")).toBe(true);
       done();
     });
   });
 });
 
-describe('ids match', () => {
-  ids.forEach( ([scenario, snippet]) => {
-    test(scenario, (done) => {
-      expect(syntaxCheck(snippet, 'id')).toBe(true);
+describe("ids match", () => {
+  ids.forEach(([scenario, snippet]) => {
+    test(scenario, done => {
+      expect(syntaxCheck(snippet, "id")).toBe(true);
       done();
     });
   });
 });
 
-describe('Primary matches', () => {
-  Primaries.forEach( ([scenario, snippet]) => {
-    test(scenario, (done) => {
-      expect(syntaxCheck(snippet, 'Primary')).toBe(true);
+describe("Primary matches", () => {
+  Primaries.forEach(([scenario, snippet]) => {
+    test(scenario, done => {
+      expect(syntaxCheck(snippet, "Primary")).toBe(true);
       done();
     });
   });
 });
 
-describe('Term matches', () => {
-  Terms.forEach( ([scenario, snippet]) => {
-    test(scenario, (done) => {
-      expect(syntaxCheck(snippet, 'Term')).toBe(true);
+describe("Term matches", () => {
+  Terms.forEach(([scenario, snippet]) => {
+    test(scenario, done => {
+      expect(syntaxCheck(snippet, "Term")).toBe(true);
       done();
     });
   });
 });
 
-describe('interpolation matches', () => {
-  interpolations.forEach( ([scenario, snippet]) => {
-    test(scenario, (done) => {
-      expect(syntaxCheck(snippet, 'strlit')).toBe(true);
+describe("interpolation matches", () => {
+  interpolations.forEach(([scenario, snippet]) => {
+    test(scenario, done => {
+      expect(syntaxCheck(snippet, "strlit")).toBe(true);
       done();
     });
   });
--- grammar/__tests__/grammar_errors.test.js
+++ grammar/__tests__/grammar_errors.test.js
@@ -5,74 +5,88 @@
  * syntax errors.
  */
 
-const syntaxCheck = require('../syntax-checker');
+const syntaxCheck = require("../syntax-checker");
 
 const miscellaneous_errors = [
-  ['does not say incorrect string literals', 'say "Hello, World!;', 'Statement'],
-  ['unknown operator', 'x := 2 ** 5', 'Statement'],
-  ['chained relational operators', '1 is less than 3 is less than 5', 'Statement'],
-  ['mismatched parentheses', '1 + (2 +3))', 'Statement']
+  [
+    "does not woof incorrect string literals",
+    'woof "Hello, World!;',
+    "Statement"
+  ],
+  ["unknown operator", "x := 2 ** 5", "Statement"],
+  ["chained relational operators", "1 isLessThan 3 isLessThan 5", "Statement"],
+  ["mismatched parentheses", "1 + (2 +3))", "Statement"]
 ];
 
 const non_comments = [
-  ['no ending tag','!!! This is not a comment'],
-  ['ending tag in middle of comment', '!!! This is also !!! not quite a comment'],
-  ['no starting tag', "You know what I'm not !!!"],
-  ['just characters without tags', 'hello I am the comment your mom told you to avoid']
+  ["no ending tag", "!!! This is not a comment"],
+  [
+    "ending tag in middle of comment",
+    "!!! This is also !!! not quite a comment"
+  ],
+  ["no starting tag", "You know what I'm not !!!"],
+  [
+    "just characters without tags",
+    "hello I am the comment your mom told you to avoid"
+  ]
 ];
 
 const non_ids = [
-  ['keyword', 'is'],
-  ['starts with number', '23jordan'],
-  ['no letters', '___888'],
-  ['bad character', '$potato']
+  ["keyword", "is"],
+  ["starts with number", "23jordan"],
+  ["no letters", "___888"],
+  ["bad character", "$potato"]
 ];
 
 const non_Primaries = [
-  ['empty parens', '()'],
-  ['list with no closing ]', `["CeCe", "Fluffy", "Mr. Dog"`],
-  ['map with no closing ]', `["CeCe": "cutest", "Marcy": "cute", "Marvin": "barely passing"`],
-  ['map with nothing on right', '["MyDog":]'],
-  ['map with nothing on left', '[:"MyDog"]']
-]
+  ["empty parens", "()"],
+  ["list with no closing ]", `["CeCe", "Fluffy", "Mr. Dog"`],
+  [
+    "map with no closing ]",
+    `["CeCe": "cutest", "Marcy": "cute", "Marvin": "barely passing"`
+  ],
+  ["map with nothing on right", '["MyDog":]'],
+  ["map with nothing on left", '[:"MyDog"]']
+];
 
 const non_Terms = [
-  ['factorial with ! before number', '!23'],
-  ['binary operator before terms', '+ 8 9'],
-  ['two operators together', '8 + mod 9']
-]
+  ["factorial with ! before number", "!23"],
+  ["binary operator before terms", "+ 8 9"],
+  ["unary operator treated as a binary operator", "6 ! 3"],
+  [("two operators together", "8 + mod 9")]
+];
 
-describe('The syntax checker', () => {
+describe("The syntax checker", () => {
   miscellaneous_errors.forEach(([scenario, program, startPoint]) => {
-    test(`detects the error ${scenario}`, (done) => {
+    test(`detects the error ${scenario}`, done => {
       expect(syntaxCheck(program, startPoint)).toBe(false);
       done();
     });
   });
 });
 
-describe('reject comments', () => {
+describe("reject bad comments", () => {
   non_comments.forEach(([scenario, program]) => {
-    test(`${scenario}`, (done) => {
-      expect(syntaxCheck(program, 'comment')).toBe(false);
+    test(`${scenario}`, done => {
+      expect(syntaxCheck(program, "comment")).toBe(false);
       done();
     });
   });
 });
 
-describe('reject Primary', () => {
+describe("reject bad Primary", () => {
   non_Primaries.forEach(([scenario, program]) => {
-    test(`${scenario}`, (done) => {
-      expect(syntaxCheck(program, 'Primary')).toBe(false);
+    test(`${scenario}`, done => {
+      expect(syntaxCheck(program, "Primary")).toBe(false);
       done();
     });
   });
 });
 
-describe('reject Terms', () => {
+describe("reject bad Terms", () => {
   non_Terms.forEach(([scenario, program]) => {
-    test(`${scenario}`, (done) => {
-      expect(syntaxCheck(program, 'Term')).toBe(false);
+    test(`${scenario}`, done => {
+      expect(syntaxCheck(program, "Term")).toBe(false);
       done();
     });
   });
