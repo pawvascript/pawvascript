7472adc64b5943ee8501fe5cfe6ad4f5247ceba6
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2020-02-13 11:57:08 -0800

    Convert to prettier

--- grammar/syntax-checker.js
+++ grammar/syntax-checker.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const ohm = require('ohm-js');
+const fs = require("fs");
+const ohm = require("ohm-js");
 
-const grammar = ohm.grammar(fs.readFileSync('grammar/pawvascript.ohm'));
+const grammar = ohm.grammar(fs.readFileSync("grammar/pawvascript.ohm"));
 
-module.exports = (text, startPoint) => grammar.match(text, startPoint).succeeded();
+module.exports = (text, startPoint) =>
+  grammar.match(text, startPoint).succeeded();
