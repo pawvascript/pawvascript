a8c0c1463352d7327e0867d330e36071e910ff49
Moriah Tolliver <mtollive@lion.lmu.edu>
2020-04-12 20:36:58 -0700

    add commented out tests in new places

--- semantics/__tests__/analyzer_errors.test.js
+++ semantics/__tests__/analyzer_errors.test.js
@@ -104,6 +104,25 @@ const errors = [
       trick DogOwner chews [leash:dogName] fetches Owner;
     tail`
   ],
+    ["use of undeclared variable", String.raw`dogName is "Mr. Fluffington";`],
+  [
+    "redeclaration of a variable",
+    String.raw`leash dogName is "Mr. Fluffington";
+  leash dogName is "Dr. Fluffington";`,
+  ],
+  [
+    "mismatch types for number declaration",
+    String.raw`toeBeans CeCeAge; CeCeAge is "12";`,
+  ],
+  [
+    "mismatch types for boolean declaration",
+    String.raw`goodBoy isNice is "true";`,
+  ],
+  ["mismatch types for string declration", String.raw`leash name is good;`],
+  [
+    "mismatch types for list declaration",
+    String.raw`pack[leash] dogs is [good, good, bad];`,
+  ],
   /* Functions */
   [
     "function with return type has no give statement",
@@ -141,6 +160,7 @@ const errors = [
     
     sayAge(age);`
   ],
+  ["called nonexistent function", String.raw`getDogs();`],
   /* Types */
   [
     "field id already used",
@@ -228,6 +248,21 @@ const errors = [
   [
     "list literal with mismatch types",
     String.raw`woof ["string", 1, good];`
+  ],
+  [
+    "spread with undeclared variable",
+    String.raw`pack[leash] allDogs is ["Bear", "Bermuda", peanutButter dogs];`,
+  ],
+  [
+    "spread with incompatible types",
+    String.raw`
+  pack[leash] dogNames is ["CeCe", "Fluffy", "Mike"];
+  pack[toeBeans] dogAges is [1, 2, 3];
+  pack[leash] dogInfo is [dogNames, peanutButter dogAges];`,
+  ],
+  [
+    "spread with non-spreadable data types",
+    String.raw`toeBeans dogAge is 7; pack[toeBeans] dogAges is [9, peanutButter dogAge];`,
   ],
   /* Kennels */
   [
@@ -330,203 +365,33 @@ const errors = [
     "key type used with 'of' does not match key type in dictionary",
     String.raw`kennel[leash:toeBeans] names is [2: "CeCe"];
     woof names of "2";`
-  ]
-  /* Assignment */
-//   ["use of undeclared variable", String.raw`dogName is "Mr. Fluffington";`],
-//   [
-//     "redeclaration of a variable",
-//     String.raw`leash dogName is "Mr. Fluffington";
-//   leash dogName is "Dr. Fluffington";`,
-//   ],
-//   [
-//     "mismatch types for number declaration",
-//     String.raw`toeBeans CeCeAge; CeCeAge is "12";`,
-//   ],
-//   [
-//     "mismatch types for boolean declaration",
-//     String.raw`goodBoy isNice is "true";`,
-//   ],
-//   ["mismatch types for string declration", String.raw`leash name is good;`],
-//   [
-//     "mismatch types for list declaration",
-//     String.raw`pack[leash] dogs is [good, good, bad];`,
-//   ],
-//   [
-//     "mismatch types in list elements",
-//     String.raw`pack[leash] dogs is ["CeCe", "Fluffy", 2, "Mr. Fluffy"];`,
-//   ],
-//   [
-//     "mismatch value types in dictionary declaration",
-//     String.raw`kennel[leash:toeBeans] dogs is ["Cece": "2"];`,
-//   ],
-//   [
-//     "mismatch key types in dictionary elements",
-//     String.raw`kennel[leash:toeBeans] dogs is [good: 2];`,
-//   ],
-//   [
-//     "mismatch types in pack spread",
-//     String.raw`pack[leash] allDogs is ["Bear", "Bermuda", peanutButter ["CeCe", "Buster", Dumpling]];`,
-//   ],
-//   [
-//     "function with incorrect return type",
-//     String.raw`
-//     trick returnALeash fetches leash:
-//       give 8;
-//     tail
-//   `,
-//   ],
-//   [
-//     "type declarations with two constructors",
-//     String.raw`breed Owner is:   
-//     leash dogName; 
-//     toeBeans age;
-//     trick Owner chews[leash:dogName] fetches Owner;
-//     trick Owner chews[toeBeans:age] fetches Owner;
-//   tail`,
-//   ],
-//   /* Spreads */
-//   [
-//     "spread with undeclared variable",
-//     String.raw`pack[leash] allDogs is ["Bear", "Bermuda", peanutButter dogs];`,
-//   ],
-//   [
-//     "spread with incompatible types",
-//     String.raw`
-//   pack[leash] dogNames is ["CeCe", "Fluffy", "Mike"];
-//   pack[toeBeans] dogAges is [1, 2, 3];
-//   pack[leash] dogInfo is [dogNames, peanutButter dogAges];`,
-//   ],
-//   [
-//     "spread with non-spreadable data types",
-//     String.raw`toeBeans dogAge is 7; pack[toeBeans] dogAges is [9, peanutButter dogAge];`,
-//   ],
-//   /* Loops */
-//   [
-//     "undeclared variable in loop body",
-//     String.raw`
-//   pack[toeBeans] myPack is [1,2,3,4];
-//   chase element through myPack:   
-//     woof elementInAnotherPack; 
-//   tail
-//   `,
-//   ],
-//   [
-//     "try to change loop variable in loop body",
-//     String.raw`
-//     pack[toeBeans] myPack is [1,2,3,4];
-//     chase element through myPack:   
-//       element is 7;
-//       woof elementInAnotherPack; 
-//     tail`,
-//   ],
-//   [
-//     "while loop condition is not a boolean",
-//     String.raw`
-//   chase while 7:
-//     woof 7;
-//   tail`,
-//   ],
-//   /* Function Call */
-//   ["called nonexistent function", String.raw`getDogs();`],
-//   [
-//     "called function with illegal arguments",
-//     String.raw`
-//   trick getDogs:
-//     woof "John let the dogs out";
-//   tail
-
-//   getDogs("Marlene");
-//   `,
-//   ],
-//   [
-//     "missing function arguments in call",
-//     String.raw`
-//     trick getDogs chews[toeBeans: ages]:
-//       woof ages;
-//     tail
-    
-//     getDogs();`,
-//   ],
-//   /* Break, Continue */
-//   ["break used outside of loop", String.raw`poop;`],
-//   ["continue used outside of loop", String.raw`walkies;`],
-//   /* Unary Expressions */
-//   ["using not before a non-number", String.raw`goodBoy isCute is "7"!;`],
-//   [
-//     "using minus before a non-number",
-//     String.raw`toeBeans negativeEight is -"eight";`,
-//   ],
-//   ["using not before a non-boolean", String.raw`goodBoy isTall is not 8;`],
-//   /* Binary Expressions */
-//   ["right side of + is not a number", String.raw`toeBeans age is 7 + "8";`],
-//   ["left side of - is not a number", String.raw`toeBeans age is "8";`],
-//   [
-//     "right side of | is not a boolean",
-//     String.raw`goodBoy isBig is good | "bad";`,
-//   ],
-//   [
-//     "left side of & is not a boolean",
-//     String.raw`goodBoy isCute is "hello" & good;`,
-//   ],
-//   [
-//     "used isGreaterThan with mismatch types",
-//     String.raw`goodBoy isTrue is "hello" isGreaterThan 8;`,
-//   ],
-//   [
-//     "use without on non-list",
-//     String.raw`
-//     toeBeans CeCeAge is 7;
-//     toeBeans babyCeCeAge is 2;
-//     toeBeans teenCeCeAge is CeCeAge without babyCeCeAge;
-//   `,
-//   ],
-//   [
-//     "try to use at with non-number for index",
-//     String.raw`pack[toeBeans] nums is [1,2,3,4];
-//   toeBeans myNum is nums at "2";`,
-//   ],
-//   [
-//     "try to index dictionary with at",
-//     String.raw`
-//   kennel[leash: toeBeans] stringsToNums is ["one": 1, "two": 2];
-//   toeBeans myNum is stringsToNums at 8;`,
-//   ],
-//   [
-//     "use of on a non-dictionary",
-//     String.raw`pack[toeBeans] nums is [1,2,3];
-//     toeBeans myNum is nums of 1;`,
-//   ],
-//   [
-//     "right term for of is not a key",
-//     String.raw`kennel[leash:toeBeans] nums is ["CeCe": 1, "Fluffy": 2,  "Charles": 3];
-//   toeBeans myNum is nums of 1;`,
-//   ],
-//   [
-//     "access nonexistent field",
-//     String.raw`
-//   breed DogHotel is:
-//       leash name;
-//       trick DogHotel chews[leash: name] fetches DogHotel;
-//       trick greet:
-//         woof "Welcome to our Dog Hotel";
-//       tail
-//     tail
-//     DogHotel myDogHotel is DogHotel("CeCe's Castle");
-//     myDogHotel's hello();`,
-//   ],
-//   [
-//     "access field of nonexistent breedType",
-//     String.raw`
-//   breed DogHotel is:
-//       leash name;
-//       trick DogHotel chews[leash: name] fetches DogHotel;
-//       trick greet:
-//         woof "Welcome to our Dog Hotel";
-//       tail
-//     tail
-//     DogHotel myDogHotel is DogHotel("CeCe's Castle");
-//     myDogMotel's greet();`,
-//   ],
+  ],
+  [
+    "access nonexistent field",
+    String.raw`
+  breed DogHotel is:
+      leash name;
+      trick DogHotel chews[leash: name] fetches DogHotel;
+      trick greet:
+        woof "Welcome to our Dog Hotel";
+      tail
+    tail
+    DogHotel myDogHotel is DogHotel("CeCe's Castle");
+    myDogHotel's hello();`,
+  ],
+  [
+    "access field of nonexistent breedType",
+    String.raw`
+  breed DogHotel is:
+      leash name;
+      trick DogHotel chews[leash: name] fetches DogHotel;
+      trick greet:
+        woof "Welcome to our Dog Hotel";
+      tail
+    tail
+    DogHotel myDogHotel is DogHotel("CeCe's Castle");
+    myDogMotel's greet();`,
+  ],
 ];
 
 describe("The semantic error analyzer", () => {
