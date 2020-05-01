ea0b609974f4dcb6b548a0384c60970f89d5d8da
Lucille Njoo <lucillenjoo@gmail.com>
2020-05-01 08:19:44 -0700

    Continue working on optimizer and tests

--- semantics/__tests__/optimizer.test.js
+++ semantics/__tests__/optimizer.test.js
@@ -1,14 +1,44 @@
-const parse = require("../ast/parser");
-const generate = require("../backend/javascript-generator");
-const Context = require("../semantics/context");
+const parse = require("../../ast/parser");
+const generate = require("../../backend/javascript-generator");
+const optimize = require("../../semantics/optimizer");
+const Context = require("../../semantics/context");
 
 const fixture = {
-  ifStatement: [
-    String.raw`if (bad) then:
-         give "You're bad";
-        tail`,
-    String.raw`if (bad) then: tail`,
+  firstIfIsTrue: [
+    String.raw`if (good) then:
+         woof "You're good";
+    else:
+        woof "You're bad";
+    tail`,
+    /if \(true\) \{\s*console.log\(`You're good`\);\s*\}/,
   ],
+  firstIfIsFalse: [
+    String.raw`if (bad) then:
+           woof "You're bad";
+      else:
+          woof "You're good";
+      tail`,
+    /if \(false\) {} else {\s*console.log\(`You're good`\);\s*}/,
+  ],
+  forLoop: [
+    String.raw`chase toeBeans i is 0 by i+1 while i isGreaterThan 2:
+          woof "hi";
+      tail`,
+    "",
+  ],
+  whileLoop: [
+    String.raw`chase while 0 equals 2:
+          woof 1;
+      tail`,
+    "",
+  ],
+  fixedLoop: [
+    String.raw`chase 0 times:
+          woof 2;
+      tail`,
+    "",
+  ],
+  variableExpression: [String.raw`toeBeans x is 5 +  10;`, /let x_(\d+) = 15;/],
 };
 
 describe("The optimized JavaScript generator", () => {
--- semantics/optimizer.js
+++ semantics/optimizer.js
@@ -13,7 +13,6 @@ const {
   BreedType,
   Field,
   Method,
-  PrimitiveType,
   ListType,
   DictType,
   IdType,
@@ -52,10 +51,12 @@ function isOne(e) {
 }
 
 function isSameNumber(e) {
-  return e.left instanceof NumberLiteral && e.right instanceof NumberLiteral && e.left.value === e.right.value;
+  return (
+    e.left instanceof NumberLiteral &&
+    e.right instanceof NumberLiteral &&
+    e.left.value === e.right.value
+  );
 }
-
-
 
 function isFalse(b) {
   return b instanceof BooleanLiteral && b.value === false;
@@ -83,7 +84,26 @@ function toUpperCase(e) {
   return e.toUpperCase();
 }
 
-const factorialTable = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, ‬39916800‬, 479001600];
+// If the given type is an IdType, returns type.ref; otherwise, returns the given type
+function getTypeReference(type) {
+  return type.constructor === IdType ? type.ref : type;
+}
+
+const factorialTable = [
+  1,
+  1,
+  2,
+  6,
+  24,
+  120,
+  720,
+  5040,
+  40320,
+  362880,
+  3628800,
+  39916800,
+  479001600,
+];
 function lookupFactorial(n) {
   if (n >= 0 && n <= 12) {
     return factorialTable[n];
@@ -94,17 +114,13 @@ function lookupFactorial(n) {
 function factorial(n) {
   return n != 1 ? n * factorial(n - 1) : 1;
 }
-// "cat"  1 quasi: ["cat"], 0 exp: []
-// a = "cat1 ![name1]"  1 quasi ["cat1 ""], 1 exp
-// b = "![name2] cat2"  2 quasis ["", " cat2"], 1 exp
-// a with b:
-// "cat1 ![name1]![name2] cat2"   quasis: ["cat1 ", "", " cat2"], exps:
 
 // ArrayExp.prototype.optimize = function() {
 //   this.size = this.size.optimize();
 //   this.fill = this.fill.optimize();
 //   return this;
 // };
+// TODO ask toal:
 // is it an optimization to deconstruct spreads on lists?
 
 Program.prototype.optimize = function() {
@@ -113,15 +129,41 @@ Program.prototype.optimize = function() {
 };
 
 Block.prototype.optimize = function() {
-  this.statements = this.statements.map((statement) => statement.optimize()).filter(statement => statement !== null);
+  this.statements = this.statements
+    .map((statement) => statement.optimize())
+    .filter((statement) => statement !== null);
+  // If we find a return, break, or continue statement, then we can remove
+  // all statements after that one since they will never execute anyways
+  const stoppingIndex = this.statements.findIndex(
+    (s) =>
+      s.constructor === GiveStatement ||
+      s.constructor === BreakStatement ||
+      s.constructor === ContinueStatement
+  );
+  if (stoppingIndex > -1 && stoppingIndex < this.statements.length - 1) {
+    this.statements.splice(
+      stoppingIndex + 1,
+      this.statements.length - (stoppingIndex + 1)
+    );
+  }
+  //  todo ask toal how to implement this:
+  // look for variable declarations; if not used in block, remove vardec??
   return this;
 };
 
 ConditionalStatement.prototype.optimize = function() {
   this.condition = this.condition.optimize();
-  if (isFalse(this.condition)) return null;
+  if (isTrue(this.condition)) {
+    this.body = this.body.optimize();
+    this.otherwise = null;
+    return this;
+  }
+  if (isFalse(this.condition)) {
+    this.body = new Block([]);
+    this.otherwise = this.otherwise ? this.otherwise.optimize() : null;
+    return this;
+  }
   this.body = this.body.optimize();
-  if (isTrue(this.condition) || !this.otherwise) return this;
   this.otherwise = this.otherwise ? this.otherwise.optimize() : null;
   return this;
 };
@@ -133,7 +175,7 @@ InfiniteLoopStatement.prototype.optimize = function() {
 
 ForLoopStatement.prototype.optimize = function() {
   this.condition = this.condition.optimize();
-  if (isFalse(this.condition)) return;
+  if (isFalse(this.condition)) return null;
   this.localVarDec = this.localVarDec.optimize();
   this.loopExp = this.loopExp.optimize();
   this.body = this.body.optimize();
@@ -168,22 +210,25 @@ VariableDeclaration.prototype.optimize = function() {
 
 Variable.prototype.optimize = function() {
   this.initializerExp = this.initializerExp.optimize();
+  this.type = getTypeReference(this.type);
+  return this;
 };
 
 FunctionDeclaration.prototype.optimize = function() {
   this.func = this.func.optimize();
+  return this;
 };
 
 Function.prototype.optimize = function() {
   this.parameters = this.parameters.optimize();
-  this.returnType = this.returnType.optimize();
+  this.returnType = getTypeReference(this.returnType);
   this.body = this.body.optimize();
   return this;
 };
 
 Parameters.prototype.optimize = function() {
+  this.types = this.types.map((t) => getTypeReference(t));
   return this;
-  // need anything else here at all?
 };
 
 TypeDeclaration.prototype.optimize = function() {
@@ -192,11 +237,9 @@ TypeDeclaration.prototype.optimize = function() {
 };
 
 BreedType.prototype.optimize = function() {
-  this.fields = this.fields.map((field) => field.optimize());
-  this.methods = this.methods.map((method) => method.optimize());
-  this.constructors = this.constructors.map((constructorDec) =>
-    constructorDec.optimize()
-  );
+  this.fields = this.fields.map((f) => f.optimize());
+  this.methods = this.methods.map((m) => m.optimize());
+  this.constructors = this.constructors.map((c) => c.optimize());
   return this;
 };
 
@@ -208,7 +251,7 @@ ConstructorDeclaration.prototype.optimize = function() {
 
 Constructor.prototype.optimize = function() {
   this.parameters = this.parameters ? this.parameters.optimize() : null;
-  this.returnType = this.returnType ? this.returnType.optimize() : null;
+  this.returnType = getTypeReference(this.returnType);
 };
 
 Field.prototype.optimize = function() {
@@ -220,23 +263,18 @@ Method.prototype.optimize = function() {
   this.func = this.func.optimize();
 };
 
-PrimitiveType.prototype.optimize = function() {
-  this.type = this.type.optimize();
-  return this;
-};
-
 ListType.prototype.optimize = function() {
-  this.memberType = this.memberType.optimize();
+  this.memberType = getTypeReference(this.memberType);
   return this;
 };
 
 DictType.prototype.optimize = function() {
-  this.keyType = this.keyType.optimize();
-  this.valueType = this.valueType.optimize();
+  this.keyType = getTypeReference(this.keyType);
+  this.valueType = getTypeReference(this.valueType);
 };
 
 IdType.prototype.optimize = function() {
-  this.name = this.name.optimize();
+  return this.ref;
 };
 
 AssignmentStatement.prototype.optimize = function() {
@@ -257,6 +295,9 @@ FunctionCall.prototype.optimize = function() {
 PrintStatement.prototype.optimize = function() {
   this.expression = this.expression.optimize();
   //TODO: 90% sure this won't work but YOLO
+  // TODO ask toal if this is a good optimization
+  // we would basically replace all bark statements with woof statements
+  // but how to convert to uppercase if there are expressions interpolated?
   if (this.flavor === "bark") this.expression = toUpperCase(this.expression);
   return this;
 };
@@ -295,17 +336,18 @@ StringLiteral.prototype.optimize = function() {
 TemplateLiteral.prototype.optimize = function() {
   // wonder if we even need these two statements because exps can only be id's, and quasis can only be string literals
   this.quasis = this.quasis.map((quasi) => quasi.optimize());
-  this.exps = this.exps.map((exp) => exp.optimize());
+  this.exps = this.exps ? this.exps.map((exp) => exp.optimize()) : null;
   return this;
 };
 
 PackLiteral.prototype.optimize = function() {
-  this.elements = this.elements.map((element) => element.optimize());
+  this.elements = this.elements.map((e) => e.optimize());
   return this;
 };
 
 ListElement.prototype.optimize = function() {
   this.value = this.value.optimize();
+  this.type = getTypeReference(this.type);
   return this;
 };
 
@@ -318,13 +360,18 @@ KennelLiteral.prototype.optimize = function() {
 
 KeyValuePair.prototype.optimize = function() {
   this.key = this.key.optimize();
+  this.keyType = getTypeReference(this.keyType);
   this.value = this.value.optimize();
+  this.valueType = getTypeReference(this.valueType);
   return this;
 };
 
 VariableExpression.prototype.optimize = function() {
+  this.type = getTypeReference(this.type);
   // TODO ask toal:
   // wonder if it's valid code optimization to remove in-between references?
+  // answer: yes
+  // but what about here?
   return this;
 };
 
@@ -359,38 +406,44 @@ BinaryExpression.prototype.optimize = function() {
   if (this.op === "/" && isOne(this.right)) return this.left;
   if (this.op === "/" && isZero(this.left)) return new NumberLiteral(0);
   if (this.op === "mod" && isZero(this.left)) return new NumberLiteral(0);
-  if ((this.op === "isGreaterThan" || this.op === "isLessThan") && isSameNumber(this)) return new BooleanLiteral(false);
-  if (this.op === "&" && (isFalse(this.left) || isFalse(this.right))) return new BooleanLiteral(false);
-  if (this.op === "|" && (isTrue(this.left) || isTrue(this.right))) return new BooleanLiteral(true);
+  if (
+    (this.op === "isGreaterThan" || this.op === "isLessThan") &&
+    isSameNumber(this)
+  )
+    return new BooleanLiteral(false);
+  if (this.op === "&" && (isFalse(this.left) || isFalse(this.right)))
+    return new BooleanLiteral(false);
+  if (this.op === "|" && (isTrue(this.left) || isTrue(this.right)))
+    return new BooleanLiteral(true);
   if (bothNumberLiterals(this)) {
     const [x, y] = [this.left.value, this.right.value];
     if (this.op === "+") return new NumberLiteral(x + y);
     if (this.op === "-") return new NumberLiteral(x - y);
     if (this.op === "*") {
       if (x % 2 === 0) {
-        return new NumberLiteral(y << x / 2)
+        return new NumberLiteral(y << (x / 2));
       }
       if (y % 2 === 0) {
-        return new NumberLiteral(x << y / 2)
+        return new NumberLiteral(x << (y / 2));
       }
       return new NumberLiteral(x * y);
     }
     if (this.op === "/") {
       if (x % 2 === 0) {
-        return new NumberLiteral(y >> x / 2)
+        return new NumberLiteral(y >> (x / 2));
       }
       if (y % 2 === 0) {
-        return new NumberLiteral(x >> y / 2)
+        return new NumberLiteral(x >> (y / 2));
       }
       return new NumberLiteral(x / y);
     }
     if (this.op === "mod") return new NumberLiteral(x % y);
-    if (this.op === "isGreaterThan") return new NumberLiteral(x > y);
-    if (this.op === "isAtLeast") return new NumberLiteral(x >= y);
-    if (this.op === "isAtMost") return new NumberLiteral(x <= y);
-    if (this.op === "isLessThan") return new NumberLiteral(x < y);
-    if (this.op === "equals") return new NumberLiteral(x === y);
-    if (this.op === "notEquals") return new NumberLiteral(x !== y);
+    if (this.op === "isGreaterThan") return new BooleanLiteral(x > y);
+    if (this.op === "isAtLeast") return new BooleanLiteral(x >= y);
+    if (this.op === "isAtMost") return new BooleanLiteral(x <= y);
+    if (this.op === "isLessThan") return new BooleanLiteral(x < y);
+    if (this.op === "equals") return new BooleanLiteral(x === y);
+    if (this.op === "notEquals") return new BooleanLiteral(x !== y);
   }
   if (bothTemplateLiterals(this)) {
     if (this.op === "with")
@@ -406,15 +459,6 @@ BinaryExpression.prototype.optimize = function() {
   }
   return this;
 };
-
-// this is interesting -- if only 1 item in list, just return the one item rather than the list of 1
-// ExpSeq.prototype.optimize = function() {
-//   this.exps = this.exps.map((s) => s.optimize());
-//   if (this.exps.length === 1) {
-//     return this.exps[0];
-//   }
-//   return this;
-// };
 
 // IfExp.prototype.optimize = function() {
 //   this.test = this.test.optimize();
@@ -434,17 +478,5 @@ BinaryExpression.prototype.optimize = function() {
 // SubscriptedExp.prototype.optimize = function() {
 //   this.array = this.array.optimize();
 //   this.subscript = this.subscript.optimize();
-//   return this;
-// };
-
-// TODO ask toal:
-// again do we just return nothing or do we return null or nil or whatever?
-// WhileExp.prototype.optimize = function() {
-//   this.test = this.test.optimize();
-//   if (this.test instanceof Literal && !this.test.value) {
-//     // While-false is a no-operation, don't even need the body
-//     return new Nil();
-//   }
-//   this.body = this.body.optimize();
 //   return this;
 // };
